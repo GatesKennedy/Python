2be3ce8ab2a7d728922576c3735c2519a395778d
GatesKennedy <conor@esccollective.com>
2019-03-21 18:20:50 -0700

    Create: Apps: Accounts,Events,Home,Messages,Scraper

---
+++ AllofEverything/AppEvents/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppeventsConfig(AppConfig):
+    name = 'AppEvents'
---
+++ AllofEverything/AppScraper/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppscraperConfig(AppConfig):
+    name = 'AppScraper'
