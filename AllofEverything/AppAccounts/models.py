dca579cb6f288a243b964f92a6a19c5a644c443a
GatesKennedy <conor@esccollective.com>
2019-03-26 05:34:08 -0700

    Mod: AppAccounts and AppHello-Add User Models

--- AllofEverything/AppAccounts/models.py
+++ AllofEverything/AppAccounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length = 20)
+    user_Id = models.CharField(max_length = 40)
+    user_Loc = models.ForeignKey(max_length = 40)
+    user_ModList = models.CharField(max_length = 40)
--- AllofEverything/AppHello/models.py
+++ AllofEverything/AppHello/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     user_name = models.CharField(max_length = 20)
+    user_password = models.CharField(max_length = 40)
     user_Id = models.CharField(max_length = 40)
     user_Loc = models.ForeignKey(max_length = 40)
     user_ModList = models.CharField(max_length = 40)
     
+class Input(models.Model):
+    input_name = models.CharField(max_length = 20)
+    input_password = models.CharField(max_length = 40)
