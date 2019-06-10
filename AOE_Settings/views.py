2f2257fa9ca61a1e77cabaee52be1b50fe2ab6ba
GatesKennedy <conor@esccollective.com>
2019-06-09 19:33:13 -0700

    MOD: nav.html base.html, Config: navBar into base.html

--- AOE_Settings/views.py
+++ AOE_Settings/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, settings.html, context=None)
+    return render(request, 'settings.html', context=None)
     #return HttpResponse("This is where you decide")
